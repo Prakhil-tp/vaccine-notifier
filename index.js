b6e4fdeb9d23c9f57c8c34afba3fe16100b9ecff
prakhil-tp <prakhil.tp@gmail.com>
2021-07-02 22:45:17 +0530

    feat: Refactor and optimise the code

--- index.js
+++ index.js
@@ -1,66 +1,71 @@
 const rp = require("request-promise");
 const moment = require("moment");
 const { exec } = require("child_process");
+const config = require("./config.json")
 
 const date = moment().format("DD-MM-YYYY");
-const reqOptions = {
-  url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=303&date=${date}`,
-  headers: {
-    accept: "application/json, text/plain, */*",
-    "accept-language": "en-US,en;q=0.9",
-    authorization: "Bearer ",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "sec-gpc": "1"
-  },
-  referrer: "https://selfregistration.cowin.gov.in/",
-  referrerPolicy: "strict-origin-when-cross-origin",
-  body: null,
-  method: "GET",
-  mode: "cors"
-};
+config.url += `?district_id=303&date=${date}`
 
-(async () => {
-  const dataStr = await rp(reqOptions);
-  const { centers } = JSON.parse(dataStr);
+/**
+ * @function makeMessage
+ * @param {object} center - vaccine center
+ * @param {string} vaccine - vaccine name
+ * @param {string} date - available date
+ * @returns {string} - A message with vaccine center details
+ */
+const makeMessage = (center, vaccine, date) => {
+  return (
+    `
+    Center: ${center.name}
+    Address: ${center.address}
+    Block: ${center.block_name}
+    Pincode: ${center.block_name}
+    Fee type: ${center.fee_type}
+    Vaccine: ${vaccine}
+    Date: ${date}
 
-  const availability = centers.filter((center) => {
-    return center.sessions.some((session) => session.available_capacity > 0);
+    https://selfregistration.cowin.gov.in/
+
+    `
+  )
+}
+
+/**
+ * @function sendMessage - execute shell script to send the message
+ * @param {string} - A message with vaccine available centers' details
+ */
+const sendMessage = (message) => {
+  exec(`./script.sh "${message}"`, (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (err !== null) {
+      console.log(`exec error: ${err}`);
+    }
   });
+}
 
-  if (availability.length > 0) {
-    let message = `
-    VACCINE AVAILABILE
-  `;
-    message += availability.reduce((acc, value) => {
-      let covaxin = "";
-      let covishield = "";
+/**
+ * IIFE - Fetch data from the CDN and find available centers
+ */
+(async () => {
+  const dataStr = await rp(config);
+  const { centers } = JSON.parse(dataStr);
+  const availableCenters = centers.filter((center) => {
+    return center.sessions.some((session) => session.available_capacity_dose1 > 0 && session.min_age_limit === 18);
+  }); // O(n*m)
 
-      if (value.sessions.some((session) => session.vaccine === "COVAXIN")) {
-        covaxin = "COVAXIN";
-      } else if (
-        value.sessions.some((session) => session.vaccine === "COVISHIELD")
-      ) {
-        covishield = "COVISHIELD";
-      }
-      value = `
-    center name: ${value.name} 
-    address: ${value.address}
-    block name: ${value.block_name}
-    pincode: ${value.block_name}
-    fee type: ${value.fee_type}
-    vaccine: ${covishield ? covishield + "," : ""} ${covaxin}
-    `;
-      return acc + value;
-    }, "");
-
-    exec(`./script.sh "${message}"`, (err, stdout, stderr) => {
-      console.log(stdout);
-      console.log(stderr);
-      if (err !== null) {
-        console.log(`exec error: ${err}`);
-      }
-    });
+  if (availableCenters.length > 0) {
+    let message = " VACCINE AVAILABLE";
+    message += availableCenters.reduce((acc, value) => {
+      let vaccine = []
+      let date = [];
+      value.sessions.forEach(session => {
+        if(!vaccine.some(session.vaccine)) vaccine.push(session.vaccine)
+        if(!date.some(session.date)) date.push(session.date)
+      })
+      return acc + makeMessage(value, vaccine.toString(), date.toString())
+    }, ""); // O(p*m)
+    sendMessage(message);
   }
 })();
+// O(m*(p+n))
