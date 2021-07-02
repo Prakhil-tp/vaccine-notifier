68777912ebdd2ffe1b1d430a132a0618f6169da7
prakhil-tp <prakhil.tp@gmail.com>
2021-07-03 00:04:52 +0530

    Feat: update log message

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Program running"
+echo "Program running - `date -u`"
 cd /home/ubuntu/vaccine-notifier/
 /home/ubuntu/.nvm/versions/node/v15.2.1/bin/node /home/ubuntu/vaccine-notifier/index.js 
 
